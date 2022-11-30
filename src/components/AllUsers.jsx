84cb4dd23adcaaf4e2efe85bf8c769bbd5e2e599
Momen-Ajarmah <m2men1999@gmail.com>
2022-11-30 23:04:46 +0200

    Edit the table

--- src/components/AllUsers.jsx
+++ src/components/AllUsers.jsx
@@ -24,30 +24,31 @@ function AllUsers() {
        
   return (
     <>
-        <div className="input-group mb-3 search">
+    <div className="d-flex flex-column align-items-center ">
+
+        <div className="input-group  search d-flex align-items-st">
           <div className="input-group-prepend">
             <span className="input-group-text" id="basic-addon1"> Search for Someone</span>
           </div>
           <input type="text" onChange={e=>setQuery(e.target.value)}   className="form-control"  placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"  />
         </div>
-        <table className="table w-50">
+        <table className="table w-50 ">
             <thead>
                 <tr>
-                    <th scope="col">Name</th>
+                    <th className='text-center' scope="col">Name</th>
                 </tr>
             </thead>
             
                     {
                         allUsers.filter(user=>user.userName.toLowerCase().includes(query)).map((user,id)=>
                         <tbody>
-                        <td> {user.userName}</td>
-                        <td key={user._id} onClick={()=> sendToProfile( user._id )}><button type="button" class="btn btn-info">Send a Message</button></td>
+                        <td className='w-50 text-center'> {user.userName}</td>
+                        <td className='w-50' key={user._id} onClick={()=> sendToProfile( user._id )}><button type="button" class="btn btn-info w-50">Send a Message</button></td>
                         </tbody>
                         )
                     }
-            
-           
         </table>
+    </div>
 
     </>
   )
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,8 +12,8 @@ function Login({ setUserData }) {
   let [errorMsg, setErrorMsg] = useState("");
 
   let navigate = useNavigate();
-  function goToSendMessage() {
-    let path = "/sendMessage";
+  function ReceivedMessages() {
+    let path = "/ReceivedMessages";
     navigate(path);
   }
   let submitFormData = async (e) => {
@@ -24,7 +24,7 @@ function Login({ setUserData }) {
       setLoading(true);
       localStorage.setItem("token", data.loginToken);
       setUserData();
-      goToSendMessage();
+      ReceivedMessages();
       
     } else {
       setLoading(false);
